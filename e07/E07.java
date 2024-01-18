194f82b6b97c8c3cb1830876866ba56f4f70e9c6
GebremariamFikre <153544690+GebremariamFikre@users.noreply.github.com>
2024-01-18 10:20:02 +0200

    Update E07.java

--- e07/E07.java
+++ e07/E07.java
@@ -1,5 +1,14 @@
 class E07 {
     public static void main(String[] args) {
-
+     Scanner in = new Scanner(System.in);
+        System.out.println("Which multiplicand multiplication table to show?");
+        int productNum =in.nextInt();
+        System.out.println("Until which multiplier?");
+        int multiplier = in .nextInt();
+        System.out.println();
+        for (int i = 1; i <= multiplier; i++) {
+            int result = i * productNum;
+            System.out.println(i + " * " + productNum + " = " + result);
+        }
     }
 }
