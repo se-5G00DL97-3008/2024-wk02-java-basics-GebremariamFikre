41dd26f0c85d3677eb022b497f9914aaeb479386
GebremariamFikre <153544690+GebremariamFikre@users.noreply.github.com>
2024-01-18 11:27:10 +0200

    Update E03.java

--- e03/E03.java
+++ e03/E03.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class E03 {
+public class E03 {
     public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
         System.out.println("Enter a number:");
