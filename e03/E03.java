fb1ae7a89c8de9ea41d5d35e1a96d0457dd985bd
GebremariamFikre <153544690+GebremariamFikre@users.noreply.github.com>
2024-01-18 11:31:29 +0200

    Update E03.java

--- e03/E03.java
+++ e03/E03.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
 public class E03 {
     public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.println("Enter a number:");
         System.out.println();
         int num = in.nextInt();
         System.out.println();
         if (num > 0){
             System.out.println("Positive number");
-            }
-        System.out.println();
+        }
         System.out.println("Enter a number:");
         System.out.println();
         num = in.nextInt();
         System.out.println();
         if (num < 0) {
-                System.out.println("Negative number");
-            }
+            System.out.println("Negative number");
+        }
     }
 }
