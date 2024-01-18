7e4f565227c1a491c5fbb245701911a2cdedc2df
GebremariamFikre <153544690+GebremariamFikre@users.noreply.github.com>
2024-01-18 10:46:07 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 class E02 {
-    public static void main(String[] args) {
-   Scanner in =new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner in =new Scanner(System.in);
         System.out.println("Give a number:");
+
         int num1 = in.nextInt();
         System.out.println("Give another number");
         int num2 = in.nextInt();
@@ -17,4 +18,5 @@ class E02 {
         System.out.println(num1 + " / " + num2 + " = " + division);
 
     }
+
 }
