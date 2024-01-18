fb710e9ba58e128406d2144858c5f071082911cd
GebremariamFikre <153544690+GebremariamFikre@users.noreply.github.com>
2024-01-18 10:19:00 +0200

    Update E06.java

--- e06/E06.java
+++ e06/E06.java
@@ -1,5 +1,11 @@
 class E06 {
     public static void main(String[] args) {
-
+    Scanner in = new Scanner(System.in);
+        System.out.println("Which multiplication table to show?");
+        int productNum =in.nextInt();
+        for (int i = 0; i <=10; i++) {
+            int result = i * productNum;
+            System.out.println(i + " * " + productNum + " = " + result);
+        }
     }
 }
