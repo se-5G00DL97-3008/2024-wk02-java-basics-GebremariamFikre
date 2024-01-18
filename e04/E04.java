6f9116d2b736136cfa7386401ff2a05276348076
GebremariamFikre <153544690+GebremariamFikre@users.noreply.github.com>
2024-01-18 11:47:27 +0200

    Update E04.java

--- e04/E04.java
+++ e04/E04.java
@@ -3,23 +3,15 @@ public class E04 {
     public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     System.out.print("Enter a day: ");
-        System.out.println();
     int day = in.nextInt();
-        System.out.println();
     System.out.print("Enter a month: ");
-        System.out.println();
     int month = in.nextInt();
-        System.out.println();
     if (day == 24 && month == 12) {
         System.out.println("Merry Christmas");
-        System.out.println();
     }
         System.out.print("Enter a day: ");
-        System.out.println();
         day = in.nextInt();
-        System.out.println();
         System.out.print("Enter a month: ");
-        System.out.println();
         month =  in.nextInt();
     }
 }
